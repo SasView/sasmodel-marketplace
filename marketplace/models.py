1fb76b404c6e918f9d7bd94c2374ac78ea38963f
Lewis O'Driscoll <lewis.o'driscoll@stfc.ac.uk>
2016-08-25 13:53:31 +0100

    Add basic model detail page

--- marketplace/models.py
+++ marketplace/models.py
@@ -1,8 +1,11 @@
-from __future__ import unicode_literals
-
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
+@python_2_unicode_compatible
 class SasviewModel(models.Model):
     name = models.CharField(max_length=150)
     description = models.CharField(max_length=500)
     upload_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.name
--- marketplace/views.py
+++ marketplace/views.py
@@ -1,4 +1,15 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import Http404
+from .models import SasviewModel
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    latest_models = SasviewModel.objects.order_by('-upload_date')[:5]
+    context = { 'latest_models': latest_models }
+    return render(request, 'marketplace/index.html', context)
+
+def detail(request, model_id):
+    try:
+        model = SasviewModel.objects.get(pk=model_id)
+    except SasviewModel.DoesNotExist:
+        raise Http404("Model does not exist.")
+    return render(request, 'marketplace/detail.html', { 'model': model })
