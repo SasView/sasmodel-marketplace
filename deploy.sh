41312ecf03a3731cda2a1e1ee32d31ed50f4adcc
Lewis O'Driscoll <lewis.o'driscoll@stfc.ac.uk>
2016-09-08 15:57:04 +0100

    Attempt deploy again

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 eval "$(ssh-agent -s)"
-chmod 600 .travis/deploy_key.pem
-ssh-add .travis/deploy_key.pem
-git remote add deploy www-data@danse.chem.utk.edu:/var/www/marketplace.sasview.org
+chmod 600 .travis/travis_rsa
+ssh-add .travis/travis_rsa
+git remote add deploy git@danse.chem.utk.edu:/var/www/marketplace.sasview.org
 git push deploy
