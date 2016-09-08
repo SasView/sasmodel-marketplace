812053ae6533aa94cd714bd91d47d66690009dca
Lewis O'Driscoll <lewis.o'driscoll@stfc.ac.uk>
2016-09-08 14:35:27 +0100

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 eval "$(ssh-agent -s)"
-chmod 600 deploy_key.pem
-ssh-add deploy_key.pem
+chmod 600 .travis/deploy_key.pem
+ssh-add .travis/deploy_key.pem
 git remote add deploy danse.chem.utk:/var/www/marketplace.sasview.org
 git push deploy
