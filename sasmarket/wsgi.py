8fad42dc12e85b3f3d3a7bbd2180ec975d30cf2c
Lewis O'driscol <lewis@danse.chem.utk.edu>
2016-09-08 05:56:55 -0400

    Log useful error messages in production

--- sasmarket/wsgi.py
+++ sasmarket/wsgi.py
@@ -8,9 +8,21 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
-
+import time
+import traceback
+import signal
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sasmarket.settings")
+try:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sasmarket.settings")
+    application = get_wsgi_application()
+    print "WSGI without exception"
+except:
+    print "Handling WSGI exception"
+    print "Pyton maxunicode: {}".format(sys.maxunicode)
+    if 'mod_wsgi' in sys.modules:
+        traceback.print_exc()
+        os.kill(os.getpid(), signal.SIGINT)
+        time.sleep(2.5)
 
-application = get_wsgi_application()
