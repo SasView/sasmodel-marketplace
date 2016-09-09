caac926c9086af92be4f903dbeaf20a902d0453d
Lewis O'Driscoll <lewis.o'driscoll@stfc.ac.uk>
2016-09-09 09:47:22 +0100

    Update deploy location

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 eval "$(ssh-agent -s)"
 chmod 600 .travis/travis_rsa
 ssh-add .travis/travis_rsa
-git remote add deploy git@danse.chem.utk.edu:/var/www/marketplace.sasview.org
-git push deploy
+git remote add deploy git@danse.chem.utk.edu:/home/git/marketplace.sasview.org
+git push deploy master
