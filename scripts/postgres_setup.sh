a7096d4a5ccfe966d6b542cd6ad05e8b685cf75b
Tim Snow <tim.snow@cunninglemon.com>
2018-09-08 10:12:07 +0200

    Removing easy setup database user password for travis

--- scripts/postgres_setup.sh
+++ scripts/postgres_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 psql --command "CREATE DATABASE sasmodeldatabase;"
-psql --command "CREATE USER sasmodeluser WITH PASSWORD 'sasmodeluser';"
+psql --command "CREATE USER sasmodeluser;"
 psql --command "GRANT ALL PRIVILEGES ON DATABASE sasmodeldatabase TO sasmodeluser;"
