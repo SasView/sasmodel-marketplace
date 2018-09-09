4f85fc96854cfd939f1e32b680b532a012d5c735
Tim Snow <tim.snow@cunninglemon.com>
2018-09-09 12:08:40 +0200

    Adding database files into backup

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -7,3 +7,4 @@
 BASE_WEB_DIR="/var/www/marketplace.sasview.org"
 TODAYS_DATE="$(date +%Y-%m-%d)"
 zip -9 -r --exclude=*.dropbox* --exclude=*Dropbox* --exclude=*virtualenv* $BASE_WEB_DIR/Dropbox/$TODAYS_DATE-sasmarket_backup.zip $BASE_WEB_DIR
+zip -9 -r $BASE_WEB_DIR/Dropbox/$TODAYS_DATE-psql_database_backup.zip /var/lib/postgresql
