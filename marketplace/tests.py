42fa9929a8011d2cae40a470bac7abc58b4805c8
Andrew McCluskey <andrew.mccluskey@ess.eu>
2021-01-14 13:03:54 +0100

    test an int

--- marketplace/tests.py
+++ marketplace/tests.py
@@ -91,12 +91,11 @@ class SasviewModelTests(TestCase):
         post_dict = {
             'name': model.name,
             'description': model.description,
-            'category': None
+            'category': 1
         }
         upload = open('marketplace/test_data/M1 [guinier]_out.txt', 'rb')
         file_dict = { 'example_data': SimpleUploadedFile(upload.name, upload.read()) }
         form = SasviewModelForm(post_dict, file_dict)
-        print(form.errors)
 
         self.assertTrue(form.is_valid())
         model.example_data_x = form.cleaned_data['example_data'][0]
